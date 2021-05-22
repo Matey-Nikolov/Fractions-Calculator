f48b94a0d209fc21ef58cf84f5630cb512b816b5
Matey-Nikolov <matey.nikolov@mail.bg>
2021-05-22 14:12:23 +0300

    beta 2.1

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Fractions
             {
                 string fraction = Console.ReadLine();
 
-                Regex rg = new Regex(@"[\d]{1,}\/[1-9]{1,} ((!=)?|(<=)?|(>=)?|(==)?|(<)?|(>)?|(\+)|(-)|(\/)|(\*))? [\d]{1,}\/[1-9]{1,}");
+                Regex rg = new Regex(@"\d{1,}\/[1-9]{1,} ((!=)|(<=)|(>=)|(==)|(<)|(>)|(\+)|(-)|(\/)|(\*)) \d{1,}\/[1-9]{1,}");
 
 
                 if (fraction == "END" || fraction == "end")
@@ -36,128 +36,30 @@ namespace Fractions
                     }
                     else if (fraction.Contains(" < ")) // ------------------------------------   $ < $
                     {
-                        /*
-                        string[] newFraction = fraction
-                                    .Split(new char[] { '/', '<' }, StringSplitOptions.RemoveEmptyEntries);
-
-                        int number1 = int.Parse(newFraction[0]);
-                        int number2 = int.Parse(newFraction[1]);
-
-                        Fraction drop1 = new Fraction(number1, number2);
-
-                        int number3 = int.Parse(newFraction[2]);
-                        int number4 = int.Parse(newFraction[3]);
-
-                        Fraction drop2 = new Fraction(number3, number4);
-                        */
                         Console.WriteLine(Split.SplitBylessOperation(fraction));
                     }
                     else if (fraction.Contains(" > ")) // ------------------------------------   $ > $
                     {
-                        /*
-                        string[] newFraction = fraction
-                                    .Split(new char[] { '/', '>' }, StringSplitOptions.RemoveEmptyEntries);
-
-                        int number1 = int.Parse(newFraction[0]);
-                        int number2 = int.Parse(newFraction[1]);
-
-                        Fraction drop1 = new Fraction(number1, number2);
-
-                        int number3 = int.Parse(newFraction[2]);
-                        int number4 = int.Parse(newFraction[3]);
-
-                        Fraction drop2 = new Fraction(number3, number4);
-                        */
                         Console.WriteLine(Split.SplitByBiggerOperation(fraction));
                     }
                     else if (fraction.Contains("==")) // ------------------------------------   $ == $
                     {
-                        /*
-                        string[] newFraction = fraction
-                                    .Split(new string[] { "/", "==" }, StringSplitOptions.RemoveEmptyEntries);
-
-                        int number1 = int.Parse(newFraction[0]);
-                        int number2 = int.Parse(newFraction[1]);
-
-                        Fraction drop1 = new Fraction(number1, number2);
-
-                        int number3 = int.Parse(newFraction[2]);
-                        int number4 = int.Parse(newFraction[3]);
-
-                        Fraction drop2 = new Fraction(number3, number4);
-                        */
                         Console.WriteLine(Split.SplitBy__Operation(fraction));
                     }
                     else if (fraction.Contains("<=")) // ------------------------------------   $ <= $
                     {
-                        /*
-                        string[] newFraction = fraction
-                                    .Split(new string[] { "/", "<=" }, StringSplitOptions.RemoveEmptyEntries);
-
-                        int number1 = int.Parse(newFraction[0]);
-                        int number2 = int.Parse(newFraction[1]);
-
-                        Fraction drop1 = new Fraction(number1, number2);
-
-                        int number3 = int.Parse(newFraction[2]);
-                        int number4 = int.Parse(newFraction[3]);
-
-                        Fraction drop2 = new Fraction(number3, number4);
-                        */
                         Console.WriteLine(Split.SplitByLessThanOrEqual(fraction));
                     }
                     else if (fraction.Contains(">=")) // ------------------------------------   $ >= $
                     {
-                        /*
-                        string[] newFraction = fraction
-                                    .Split(new string[] { "/", ">=" }, StringSplitOptions.RemoveEmptyEntries);
-
-                        int number1 = int.Parse(newFraction[0]);
-                        int number2 = int.Parse(newFraction[1]);
-
-                        Fraction drop1 = new Fraction(number1, number2);
-
-                        int number3 = int.Parse(newFraction[2]);
-                        int number4 = int.Parse(newFraction[3]);
-
-                        Fraction drop2 = new Fraction(number3, number4);
-                        */
                         Console.WriteLine(Split.SplitByBiggerThanOrEqual(fraction));
                     }
                     else if (fraction.Contains("!=")) // ------------------------------------   $ != $
                     {
-                        /*
-                        string[] newFraction = fraction
-                                    .Split(new string[] { "/", "!=" }, StringSplitOptions.RemoveEmptyEntries);
-
-                        int number1 = int.Parse(newFraction[0]);
-                        int number2 = int.Parse(newFraction[1]);
-
-                        Fraction drop1 = new Fraction(number1, number2);
-
-                        int number3 = int.Parse(newFraction[2]);
-                        int number4 = int.Parse(newFraction[3]);
-
-                        Fraction drop2 = new Fraction(number3, number4);
-                        */
                         Console.WriteLine(Split.SplitByNotEqual(fraction));
                     }
                     else if (fraction.Contains('/')) // ------------------------------------   $ / $
                     {
-                        /*
-                        string[] newFraction = fraction
-                                    .Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
-
-                        int number1 = int.Parse(newFraction[0]);
-                        int number2 = int.Parse(newFraction[1]);
-
-                        Fraction drop1 = new Fraction(number1, number2);
-
-                        int number3 = int.Parse(newFraction[2]);
-                        int number4 = int.Parse(newFraction[3]);
-
-                        Fraction drop2 = new Fraction(number3, number4);
-                        */
                         Console.WriteLine(Split.SplitByDivision(fraction));
                     }
                 }
